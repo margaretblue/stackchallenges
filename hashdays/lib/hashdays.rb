b35041a92d38dec1f568e10ac56887acad0c5179
Margaret Morris <margaretblue@gmail.com>
2014-02-02 15:24:37 -0800

    added user input, required date class

--- hashdays/lib/hashdays.rb
+++ hashdays/lib/hashdays.rb
@@ -1,5 +1,30 @@
-require "hashdays/version"
+#require "hashdays/version"
+require 'date'
 
-module Hashdays
-  # Your code goes here...
+class Hashday
+  attr_accessor :year, :month, :daynum
+
+  def initialize
+    get_user_input
+    @year = year
+    @month = month
+    @daynum = daynum
+    puts "You picked year:#{year} month:#{month} daynum:#{daynum}"
+    Date.new(year,month,daynum)
+  end
+
+  def get_user_input
+    puts "Year? (ex:2011) "
+    @year = gets.chomp().to_i
+    puts "Month? (ex: 2) "
+    @month = gets.chomp().to_i
+    puts "Day of that month? (ex: 3rd day would be 3) "
+    @daynum = gets.chomp.to_i
+  end
+
+  def self.two_letter_day(daynum)
+    "Su"
+  end
 end
+
+puts Hashday.new
